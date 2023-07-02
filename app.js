0ece6e881d9d6286c8faa9a3cce1d27b3d481c28
LeoAnders <leonardoanders15@gmail.com>
2023-07-02 15:24:56 -0300

    Criando e buscando por documentos no Mongoose

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const port = 3000
+const mongoose = require('mongoose');
+const path = require("path")
+const linkRoute = require("./routes/linkRoute")
+
+mongoose.connect("mongodb://localhost/newlinks", {useNewUrlParser: true,  useUnifiedTopology: true});
+
+let db = mongoose.connection
+
+db.on("error", ()=> {console.log("Error connecting to database");})
+db.once("open", ()=> {console.log("Connected to database");})
+app.use("/", linkRoute)
+
+app.listen(port, ()=> console.log(`Server running on port ${port}`))
---
+++ routes/linkRoute.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router()
+
+const linkController = require("../controllers/linkController")
+
+router.get("/:title", linkController.redirect )
+
+module.exports = router
