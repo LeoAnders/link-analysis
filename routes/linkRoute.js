60e6a37e3120708d6fe93f235ada804cb4920220
LeoAnders <leonardoanders15@gmail.com>
2023-07-02 19:19:55 -0300

    Criando edicao de link

--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -6,10 +6,12 @@ router.use(methodOverride('_method'))
 const linkController = require("../controllers/linkController")
 
 router.get("/", linkController.allLinks)
-router.get("/:title", linkController.redirect)
 router.get("/add", (req, res)=> res.render("index"))
+router.get("/:title", linkController.redirect)
+router.get('/edit/:id', linkController.loadLink)
 
 router.post("/", express.urlencoded({ extended:true }), linkController.addLink)
+router.post('/edit/:id', express.urlencoded({ extended: true }), linkController.editLink)
 
 router.delete("/:id", linkController.deleteLink)
 router.delete("/",  express.urlencoded({ extended:true }), linkController.deleteLink)
